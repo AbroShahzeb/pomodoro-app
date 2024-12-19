11e7fd5dd57f8c5988e12847a55543eaade2be78
AbroShahzeb <khizr77@gmail.com>
2024-12-19 23:05:43 +0500

    commit

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -2,10 +2,11 @@ import { Logo } from "./icon/Logo";
 
 export const AppTitle = () => {
   return (
-    <div className="w-full flex justify-center">
-      <span className="w-full flex justify-center mt-2">
-        <Logo />
-      </span>
+    <div className="w-full flex justify-center items-end">
+      <h1 className="leading-[40px] text-[32px] font-bold text-[#D7E0FF] font-theme1Font">
+        pomodoro
+      </h1>
+
       {/* <h1 className="text-[32px] leading-normal font-bold text-center text-grayTwo">
         pomodoro
       </h1> */}
